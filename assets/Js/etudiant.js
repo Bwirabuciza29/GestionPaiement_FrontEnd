a136544e745658e6c29d185b91ab5b33f6dc7d54
Bwirabuciza29 <blondybwirabuciza@gmail.com>
2024-03-23 18:24:18 +0200

    Etudiant

---
+++ assets/Js/etudiant.js
@@ -0,0 +1,26 @@
+(function () {
+
+    let btn = document.querySelector('#submit');
+    btn.addEventListener("click", function () {
+        let noms = document.querySelector('#noms').value
+        let genre = document.querySelector('#genre').value
+        let lieu = document.querySelector('#lieu').value
+        let datenaissance = document.querySelector('#datenaissance').value
+        let adresse = document.querySelector('#adresse').value
+
+        let form = new FormData()
+        form.append('noms', noms)
+        form.append('genre', genre)
+        form.append('lieu', lieu)
+        form.append('datenaissance', datenaissance)
+        form.append('adresse', adresse)
+
+        axios.post("/TP_LABO/api/etudiant/enregistrer/?user=tplabo&mdp=12345", form).then((data) => {
+            console.log(data);
+            let inputs = document.querySelectorAll("input")
+            inputs.forEach((index) => {
+                index.value = "";
+            })
+        })
+    })
+})()
